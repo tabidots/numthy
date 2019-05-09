6245608ce848ea895762397ddbd29e27a32054a2
tabidots <tabidots@gmail.com>
2019-05-09 07:15:40 +0700

    Actually upload refactored Dixon's

--- src/numthy/factorization/dixon.clj
+++ src/numthy/factorization/dixon.clj
@@ -1,35 +1,35 @@
 (ns numthy.factorization.dixon
   "Implementation of Dixon's integer factorization algorithm, which belongs to the family of congruence-of-squares methods."
   (:require [clojure.core.reducers :as r]
+            [numthy.factorization.squares-utils :refer [factorize-rhs find-factor smooth-factorization smoothness-bound]]
+            [numthy.factorization.trial-division :refer [batch-smooth-filter]]
             [numthy.helpers :refer [isqrt]]
-            [numthy.primes.is-prime :refer [prime?]]
-            [numthy.factorization.squares-utils :refer [smooth? smoothness-bound find-factor]]
-            [numthy.modular-arithmetic.utils :refer [mod-pow]]))
+            [numthy.modular-arithmetic.utils :refer [mod-pow]]
+            [numthy.primes.sieves :refer [primes-to]]))
 
 (set! *unchecked-math* true)
 (set! *warn-on-reflection* true)
 
-(defn- generate-relations
-  "Given a factor base (vector of primes) with B elements, accumulate B+1 values of z,
-  between sqrt(n) and n, s.t. z^2 mod n is smooth over the factor base. Returns a hash-map
-  with the zs as keys and the exponent vectors as vals."
-  [n factor-base]
-  (let [prime-map   (into (sorted-map) (zipmap factor-base (repeat 0)))
-        z2-Bsmooth? (fn [z]
-                      (when-let [expv (-> (mod-pow z 2 n)
-                                          (smooth? factor-base prime-map))]
-                        {z expv}))]
-    (->> (range (inc (isqrt n)) (inc n))
-         (keep z2-Bsmooth?)
-         (take (inc (count factor-base)))
-         (apply merge))))
+(defn- add-relations
+  "As per Dixon's algorithm, runs up the range [√n, n] testing for values of x
+  s.t. x^2 mod n is smooth over the factor base. Smooth relations are added to a
+  map of the form {x x^2 mod n}. This function consumes and tests candidates in
+  batches of 250."
+  [n factor-base limit]
+  (->> (partition 250 (range (inc (isqrt n)) (inc n)))
+       (r/reduce (fn [smooths xs]
+                   (if (> (count smooths) limit) (reduced smooths)
+                     (->> (batch-smooth-filter factor-base #(mod-pow % 2 n) xs)
+                          (factorize-rhs factor-base)
+                          (into smooths))))
+                 {})))
 
-(defn dixon-factorize
-  "Uses Dixon's factorization algorithm to find at least one factor of n.
+(defn dixon
+  "Uses Dixon's factorization algorithm to find one prime factor of n.
   Returns nil if algorithm fails to find a factor."
   ;; With reference to https://crypto.stanford.edu/cs359c/17sp/projects/BrendonGo.pdf
   [n]
   (let [B           (smoothness-bound n)
-        factor-base (filter prime? (range B))
-        relations   (generate-relations n factor-base)]
+        factor-base (cons -1 (primes-to (inc (int B))))
+        relations   (add-relations n factor-base (inc (count factor-base)))]
     (find-factor n factor-base relations)))
