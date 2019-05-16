572cfb44abf927285b4a99d0f142eed39b660a06
tabidots <tabidots@gmail.com>
2019-05-16 20:02:28 +0700

    Optimize choice of multiplier for CFRAC

--- src/numthy/factorization/cfrac.clj
+++ src/numthy/factorization/cfrac.clj
@@ -2,7 +2,7 @@
   "Implementation of the continued-fraction factorization algorithm, which belongs to the family of congruence-of-squares methods."
   (:require [clojure.core.matrix :as m]
             [clojure.core.reducers :as r]
-            [clojure.math.numeric-tower :refer [abs]]
+            [clojure.math.numeric-tower :refer [abs expt]]
             [numthy.factorization.squares-utils :refer [factorize-rhs find-factor smoothness-bound smooth-factorization]]
             [numthy.factorization.trial-division :refer [batch-smooth-filter]]
             [numthy.helpers :refer [isqrt]]
@@ -18,7 +18,7 @@
 ;; https://frenchfries.net/paul/factoring/theory/cont.frac.html
 
 (defn- ->small-residue
-  "If x^2 mod n ≦ bound, returns x^2 mod n. If n - x^2 mod n ≦ bound, returns -(x^2) mod n.
+  "If x^2 mod n ≤ bound, returns x^2 mod n. If n - x^2 mod n ≤ bound, returns -(x^2) mod n.
   Else, returns nil. For the CFRAC algorithm, this bound is 2√(n). "
   [x n bound]
   (let [x2 (mod-pow x 2 n)]
@@ -26,22 +26,9 @@
       (let [neg-mod (- x2 n)]
         (when (<= (- bound) neg-mod 0) neg-mod)))))
 
-(defn- find-multiplier
-  "Find the k < B that maximizes the number of primes < B of which k*n is a quadratic residue."
-  [n B]
-  (let [bound (int B)]
-    (apply max-key
-      (fn [k]
-        (->> (primes-to bound)
-             (filter (fn [p] (quadratic-residue? (* k n) p)))
-             (count)))
-      (squarefrees-to bound))))
-
 (defn- sqrt-cfrac-numerators
-  "Produces a lazy infinite sequence of numerators of the convergents of
-  the continued fraction representation of √n. Note that even though the continued
-  fraction is periodic, the numerators are not periodic. They may not be distinct,
-  however, when reduced mod n."
+  "Produces a lazy sequence of numerators of the convergents of one period of the continued fraction
+  representation of √n, reduced mod n."
   [n]
   (let [a0 (isqrt n)
         cfrac-next (fn [[m d a h0 h1]]
@@ -50,12 +37,17 @@
                            a' (quot (+' a0 m') d')
                            h' (+' (*' a' h1) h0)]
                        [m' d' a' (mod h1 n) (mod h' n)]))]
-    (lazy-seq (map peek (iterate cfrac-next [0 1 a0 1 a0])))))
+    (->> [0 1 a0 1 a0]
+         (iterate cfrac-next)
+         (rest)                         ;; drop the 0th iteration
+         (take-while #(> (second %) 1)) ;; truncate to one period of the c-frac (d = 1 marks start of cycle, incl. 0th iter)
+         (map peek)
+         (lazy-seq))))
 
 (defn- add-relations
   "Given a factor base (vector of primes) with B elements, finds at least B+1 values of x from
   the numerators of the convergents of √(n), s.t. x^2 mod n is some value r or -r mod n
-  that is [a] ≦ 2√(n) and [b] smooth over the factor base. Those values of x are then factorized
+  that is [a] ≤ 2√(n) and [b] smooth over the factor base. Those values of x are then factorized
   over the primes in the factor base and returned in a map of the form {x mod n, ̂r mod n}
   where ̂r is the residue of x^2 mod n with the least absolute value."
   [n factor-base limit]
@@ -83,8 +75,65 @@
                          (filter #(quadratic-residue? n %))
                          (cons -1))
         quota       (inc (count factor-base))]
-    (or (->> (add-relations n factor-base quota)
-             (find-factor n factor-base))
-        (let [k (find-multiplier n B)]
-          (->> (add-relations (* k n) factor-base quota)
-               (find-factor n factor-base))))))
+    (some #(->> (add-relations (*' % % n) factor-base quota)
+                (find-factor n factor-base))
+          (rest (range)))))
+
+(comment
+ "The following functions were used to refine the multiplier search. I have left them here for reference.
+ The papers all say that the best multipliers are square-free, but I have found that in a minority of
+ cases, square-free multipliers do not find a factor, and even in the majority of cases where they do,
+ the so-called 'optimal k' that maximizes the number of primes < B of which k*n is a quadratic residue
+ does not necessarily find a factor of n the fastest.
+
+ Oddly enough, a factor is always found fastest when k is a perfect square. Go figure!"
+
+ (defn- sqfree-multiplier
+   "Find the squarefree integer k < B that maximizes the number of primes < B of which k*n is a quadratic residue."
+   [n B]
+   (let [bound (int B)]
+     (apply max-key
+       (fn [k]
+         (->> (primes-to bound)
+              (filter (fn [p] (quadratic-residue? (*' k n) p)))
+              (count)))
+       (squarefrees-to bound))))
+
+ (defn- find-multiplier
+   "Find the k < log_10(n) that maximizes the number of primes < B of which k*n is a quadratic residue."
+   [n B]
+   (let [bound (int B)]
+     (apply max-key
+       (fn [k]
+         (->> (primes-to bound)
+              (filter (fn [p] (quadratic-residue? (*' k n) p)))
+              (count)))
+       (range (Math/log10 n)))))
+
+ (defn- test-multipliers
+   [n]
+   (do
+     (println (format "Finding the best k-multiplier for %d..." (biginteger n)))
+     (let [B           (smoothness-bound n)
+           factor-base (->> (primes-to (inc (int B)))
+                            (filter #(quadratic-residue? n %))
+                            (cons -1))
+           quota       (inc (count factor-base))
+           little-k    (find-multiplier n B)
+           big-k       (sqfree-multiplier n B)
+           squares     (->> (map #(*' % %) (rest (range)))
+                            (take-while #(< % big-k))
+                            vec)
+           mults       (conj squares little-k big-k)]
+       (doseq [k mults]
+         (println (format "k = %d" (biginteger k)))
+         (let [f (volatile! nil)
+               s (->> (add-relations (*' k n) factor-base quota)
+                      (find-factor n factor-base)
+                      (vreset! f)
+                      time
+                      with-out-str)]
+           (if (some? @f)
+             (println (format "Factor found! Took %s ms." (re-find #"\d[\S]*" s)))
+             (println "No factor found. :("))
+           (println "")))))))
